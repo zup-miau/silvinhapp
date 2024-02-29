1a4dfc680ad75a1d6ada84c5d9464ae2dfd16d81
zup-miau <sophiavarasquim987@gmail.com>
2024-02-29 11:40:57 -0300

    CRIAÇÂO DE COMPONENTES

---
+++ silvinhahealthapp/src/components/Form/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+export default function Form() {
+  return (
+    <View style={styles.formContent}>
+      <Text>Form</Text>
+    </View>
+  )
+}
---
+++ silvinhahealthapp/src/components/Form/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from "react-native"
+
+const styles = StyleSheet.create({
+
+    formContent: {
+        width: '100%',
+        height: '100%',
+        bottom: 0, 
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        marginTop: 30,
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30, 
+
+    }
+
+
+})
+export default styles;
